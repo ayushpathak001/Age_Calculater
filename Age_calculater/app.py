a05b47deb0094a47a295f395159362393759699d
Ayush Pathak <ayushpathak00851@gmail.com>
2026-02-20 17:06:32 +0530

    Fix formatting and add emoji to title

--- Age_calculater/app.py
+++ Age_calculater/app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from dateutil.relativedelta import relativedelta 
 from datetime import date
 
-st.title("Age Calcuator : ")
+st.title("Age Calcuator 🧮
+ ")
 
 dob = st.date_input(
     "Enter Your DOB " , 
@@ -22,3 +23,4 @@ if st.button("Check Age : ") :
 
 
     st.balloons()
+
